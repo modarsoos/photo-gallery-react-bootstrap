f69462c027e5a7510b8e5e80db60374a3deb0e60
Modar Soos <modarsoos@gmail.com>
2023-10-16 12:23:59 +0200

    sorting files

---
+++ src/components/memberInfo.js
@@ -0,0 +1,33 @@
+const memberinfo = [
+    {
+      id: 1,
+      img: 'https://source.unsplash.com/random/300×300',
+      name: 'Modar Soos',
+      position: 'Programmer',
+      phone: '+49123456789',
+      email: 'modarsoos@gmail.com',
+      website: 'https://www.modarsoos.com',
+
+    },
+    {
+      id: 2,
+      img: 'https://source.unsplash.com/random/199×300',
+      name: 'Odai Sous',
+      position: 'Son',
+      phone: '+49123456789',
+      email: 'odai@gmail.com',
+      website: 'https://www.modarsoos.com',
+
+    },
+    {
+      id: 3,
+      img: 'https://source.unsplash.com/random/200×300',
+      name: 'Bana Sous',
+      position: 'dauter',
+      phone: '+49123456789',
+      email: 'banasoos@gmail.com',
+      website: 'https://www.modarsoos.com',
+    },
+  ]
+
+  export default memberinfo
